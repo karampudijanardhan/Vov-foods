cb71a70a9b9a8a8ab5e52512f3e4c9a221fc4f1c
karampudijanardhan <janardhankarampudi@gmail.com>
2026-03-24 10:44:22 +0530

    update

--- frontend/src/components/common/Navbar.tsx
+++ frontend/src/components/common/Navbar.tsx
@@ -201,7 +201,7 @@ onClick={() => setIsMenuOpen(!isMenuOpen)}
 </div>
 
 {/* MOBILE SEARCH */}
-<div className="md:hidden px-3 pb-3">
+<div className="md:hidden px-3 pb-3 mt-4">
 <form onSubmit={handleSearch}>
 <div className="flex items-center bg-gray-100 rounded-full px-3 py-2">
 <Search className="w-5 h-5 text-gray-500"/>
